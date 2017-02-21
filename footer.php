bf624d132220dd9452471d69df4a45773b3b2965
asiancity <ath@strachwitzgerhard.de>
2017-02-21 16:34:59 +0100

    change footer menu and style footer menu

--- footer.php
+++ footer.php
@@ -22,25 +22,27 @@ $emailaddress		= get_theme_mod( 'mail' );
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer bgc-1 bdc-1" role="contentinfo">
-		<div class="information container">
-			<div class="row">
-				<div class="location col-md-4">
-					<span class="fa fa-map-marker"></span>
-					<a href="#">
-					<?php echo $address ?><br />
-					<?php echo $postcode ?> <?php echo $city ?>
-					</a>
-					<span><?php esc_html_e( 'Fax', 'gueven' ); ?> <?php echo $fax ?></span>
-				</div>
-				<div class="phone col-md-4">
-					<span class="fa fa-phone"></span>
-					<span><a href="tel:<?php echo $phoneUrl ?>"><?php echo $phone ?></a></span>
-				</div>
-				<div class="mail col-md-4"><span class="fa fa-envelope-o"></span><span><a href="mailto:<?php echo $emailaddress ?>"><?php echo $emailaddress ?></a></span></div>
-			</div>
-		</div>
-    <div class="footer-widgets">
-      <?php wp_nav_menu( array( 'theme_location' => 'menu-footer', 'menu_id' => 'footer-menu', 'menu_class' => 'clearfix' ) ); ?>
+    <div class="container">
+  		<div class="information">
+  			<div class="row">
+  				<div class="location col-md-4">
+  					<span class="fa fa-map-marker"></span>
+  					<a href="#">
+  					<?php echo $address ?><br />
+  					<?php echo $postcode ?> <?php echo $city ?>
+  					</a>
+  					<span><?php esc_html_e( 'Fax', 'gueven' ); ?> <?php echo $fax ?></span>
+  				</div>
+  				<div class="phone col-md-4">
+  					<span class="fa fa-phone"></span>
+  					<span><a href="tel:<?php echo $phoneUrl ?>"><?php echo $phone ?></a></span>
+  				</div>
+  				<div class="mail col-md-4"><span class="fa fa-envelope-o"></span><span><a href="mailto:<?php echo $emailaddress ?>"><?php echo $emailaddress ?></a></span></div>
+  			</div>
+  		</div>
+      <div class="footer-menu">
+        <?php wp_nav_menu( array( 'theme_location' => 'menu-footer', 'container_class' => 'footer-menu-container clearfix', 'menu_id' => 'footer-menu') ); ?>
+      </div>
     </div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
